c53fbdd721a057e109f205ff05c99dc711f64787
qattanmo84433 <qattanmo84433@th-nuernberg.de>
2024-08-15 00:10:16 +0200

    Improving code consistency

--- install.sh
+++ install.sh
@@ -74,13 +74,6 @@ for arg in "$@"; do
     esac
 done
 
-# Check if the --codex or -x flag was set, and if so, run pip install
-if [ "$zsh_codex_flag" = true ]; then
-    echo "Installing openai package..."
-    pip install openai --break-system-packages
-fi
-
-
 
 #############################################################################################
 ####################################### Utilities ###########################################
@@ -268,6 +261,14 @@ install_marker() {
 
 }
 
+install_codex() {
+    # Check if the --codex or -x flag was set, and if so, run pip install
+    if [ "$zsh_codex_flag" = true ]; then
+        echo "Installing openai package..."
+        pip install openai --break-system-packages
+    fi
+}
+
 install_todo() {
     if [ ! -L $HOME/.config/czsh/todo/bin/todo.sh ]; then
         logInfo "Installing todo.sh in $HOME/.config/czsh/todo\n"
@@ -398,6 +399,8 @@ install_fzf_tab
 install_marker
 
 install_todo
+
+install_codex
 
 copy_history
 
